aa320898136f62738e81fd1cbdac2706cc9b92d5
Natalia <nef.n@list.ru>
2021-07-09 15:47:04 +0300

    Initial commit

---
+++ app/src/main/java/keyone/keytwo/lesson6_fragment/MainActivity.java
@@ -0,0 +1,16 @@
+package keyone.keytwo.lesson6_fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
